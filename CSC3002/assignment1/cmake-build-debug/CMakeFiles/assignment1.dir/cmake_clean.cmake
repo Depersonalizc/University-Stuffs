48e505b50ad0c994e9b3028bfc504f4de9972fa2
Depersonalizc <1046770165@qq.com>
2020-10-24 01:37:58 +0800

    3100 a2

---
+++ CSC3002/assignment1/cmake-build-debug/CMakeFiles/assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment1.dir/main.cpp.obj"
+  "CMakeFiles/assignment1.dir/q1.cpp.obj"
+  "CMakeFiles/assignment1.dir/q2.cpp.obj"
+  "CMakeFiles/assignment1.dir/q3.cpp.obj"
+  "assignment1.exe"
+  "assignment1.exe.manifest"
+  "assignment1.lib"
+  "assignment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
